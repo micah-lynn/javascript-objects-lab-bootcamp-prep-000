b3a17d0b125ec39a3bcc56790c6a7a4a33d0a445
Micah Lynn <micah@sabado.com>
2020-01-28 16:05:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
   
-  var updateObj = Object.assign({}, object)
-  updateObj.prop = 'prop 2'
+ 
   return object
 }
 
